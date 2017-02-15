570e81c5f6e5d9462fcf2e12b1da30d67530e194
felix oporto lopez and Philip Putnam <felix@>
2017-02-15 11:28:41 -0800

    add initial files and intiliaze

---
+++ app/app.php
@@ -0,0 +1,22 @@
+<?php
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/FindReplace.php';
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+
+      return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+
+?>
